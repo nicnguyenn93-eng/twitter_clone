4e36657fc6ab91e3d35d81643f9188af2608d792
nicnguyenn93-eng <nick.nguyenn93@gmail.com>
2026-04-19 18:05:31 -0400

    first commit

--- src/tweet.tsx
+++ src/tweet.tsx
@@ -1,16 +1,33 @@
 // Tweet.js
 import React, { useState } from 'react';
 
-/* 
-Create a new componnet called 'Tweet' make sure to pass down all the props you will need.
-You will also need to use 'useState' for two variables. Then you want to create an event
-handler for when the user clicks 'like'. You can ue whatever h1s or h2s or HTML elements that 
-you want for your tweet, just make sure all the information from the screenshot I sent is there.
-Also, if the post isn't liked already, then you want to display a white heart "🤍" otherwise, 
-display a red heart "❤️" make sure to increase or decrease the like counter appropriately.
-Theres a 'tweet' class you can use to style your tweet.
-*/
+function Tweet({ username, content, likes, timestamp }) {
+    
+    const [liked, setLiked] = useState(false)
+    const [likeCount, setLikeCount] = useState(likes)
+
+    function handleLike() {
+        if (liked) {
+            setLiked(false)
+            setLikeCount(likeCount - 1)
+        } else {
+            setLiked(true)
+            setLikeCount(likeCount + 1)
+        }
+    }
+
+    return (
+        <div className="tweet">
+            <h3>@{username}</h3>
+            <p>{content}</p>
+            <p>{timestamp}</p>
+            <button onClick={handleLike}>{liked ? '❤️' : '🤍'}{likeCount}</button>
+        </div>
+    )
+
+
+}
 
 
 
-// export default Tweet;
+export default Tweet;
